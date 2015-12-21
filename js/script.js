e78edb49aaed830a0f397756381ea9e18677bd03
Bazaliy Yura <yurchykxxx@gmail.com>
2015-12-22 01:11:19 +0200

    Почти готово

--- js/script.js
+++ js/script.js
@@ -1,19 +1,6 @@
 var owl = $('.owl-carousel');
 owl.owlCarousel({
-  responsiveClass:true,
-  responsive:{
-    0:{
-      items:1,
-      nav:true
-    },
-    600:{
-      items:3,
-      nav:false
-    },
-    1000:{
-      items:5,
-      nav:true,
-      loop:false
-    }
-  }
+  nav: false,
+  items: 1,
+  dots: true
 });
